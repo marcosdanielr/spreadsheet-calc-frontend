d2ea8bfdafc1c7a6eb2d3d1508872a91fe0aafb8
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-01-24 23:51:04 -0300

    feat: upload file page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx,vue}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+     require('flowbite/plugin')
+  ],
+}
+
